1a42bfa776ccf4065985c615af41ea4983670233
lipestile <felipebalbino53@gmail.com>
2024-02-26 20:06:37 -0300

    Parte 2

---
+++ Aula1/Parte 2/02-Helloword.js
@@ -0,0 +1,6 @@
+// imprime diretamente na pagina do navegador
+document.write('Bom dia')
+// imprimir no console doo navegador, na ferrmaebta do desenvolvedor (f12)
+console.log('Boa noite Turma!')
+// imprimir em um popup
+alert('Vamos aprender Estrutura de Dados')
